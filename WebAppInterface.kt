a6613dd3f9b8695a316b16300660c7504e2ca1c2
Leonid Titov, CS <5195118@gmail.com>
2019-08-25 08:16:09 -0400

    Create WebAppInterface.kt

---
+++ WebAppInterface.kt
@@ -0,0 +1,23 @@
+package com.example.xenops_1
+
+import android.content.Context
+import android.webkit.JavascriptInterface
+import android.widget.Toast
+
+// Copyright (c) 2019 Leonid Titov, Mentions Highly Appreciated
+
+class WebAppInterface(private val mContext: Context, val mAct: MainActivity) {
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+        //  mAct.mWebView           // works
+        //  MainActivity.mWebView2  // works
+
+        mAct.mWebView?.post {
+            mAct.mWebView?.evaluateJavascript("f1();") { retv ->
+                Toast.makeText(mContext, retv, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
